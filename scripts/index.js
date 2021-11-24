f000c805abb0b3ea971c2a55a83cff68e766c40c
Ivan Baidiuk <ivan.baidiuk@ning.com>
2021-11-24 21:49:40 +0200

    home

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".big-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
